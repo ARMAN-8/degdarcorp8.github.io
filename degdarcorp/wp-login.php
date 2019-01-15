41e101e561a4387dfe90cc0d7980c2edde7d9d60
ArmanAlimnur <46555103+ArmanAlimnur@users.noreply.github.com>
2019-01-15 18:11:26 +0600

    Файлы для сайта

---
+++ degdarcorp/wp-login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="ru-RU">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="ru-RU">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>DEGDAR &lsaquo; Кіру</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='http://superlanding.kalsin.ru/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6.13' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-ru-ru">
+		<div id="login">
+		<h1><a href="https://ru.wordpress.org/" title="Сайт работает на WordPress" tabindex="-1">DEGDAR</a></h1>
+	
+<form name="loginform" id="loginform" action="http://superlanding.kalsin.ru/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Қолданушының есімі немесе e-mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Құпия сөз<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Mені есте сақтау</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Войти" />
+		<input type="hidden" name="redirect_to" value="http://superlanding.kalsin.ru/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://superlanding.kalsin.ru/wp-login.php?action=lostpassword">Құпия сөзді ұмыттыңыз ба?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://superlanding.kalsin.ru/">&larr; Сайтқа оралу &laquo;DEGDAR&raquo;</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
